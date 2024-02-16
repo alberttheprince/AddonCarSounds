50f9cae1e65859589a89aefdd17ce9d78c090305
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2024-02-16 15:51:10 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'PrinceAlbert'
+description 'Custom Car Audio'
+
+files {
+    '**/**/*.dat151.rel',
+    '**/**/*.dat54.rel',
+    '**/**/*.awc',
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/roxanne_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/roxanne_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_roxanne'
